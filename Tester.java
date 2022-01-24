097b9e13e2fb5e9b31a3371eab9514d4dfde7a22
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-01-24 09:30:01 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Tester {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		Mastermind m = new Mastermind();
-		
+		while (true) {
+			ArrayList<Character> user_choice = new ArrayList<>();
+			System.out.println("There are 6 colors in total");
+			System.out.println("select 4 color of your choice");
+			System.out.println("red = R, green = G, blue = B, purple = P, orange = O, yellow = Y");
+			System.out.println("input a single character\n");
+			
+			for (int i = 0; i < 4; i ++) {
+				System.out.print((i + 1) + " color: ");
+				user_choice.add(sc.next().charAt(0));
+			}
+			m.check_correct(user_choice);
+		}
 		
 	}
 } // end of Tester class
