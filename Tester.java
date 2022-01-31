1d5566f4874ef9271ba126ad488d2134d5f2fdc6
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-01-31 14:54:16 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+// Tester class, gather user input and run Mastermind
 public class Tester {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Mastermind m = new Mastermind();
-		while (true) {
+	public static void main(String[] args) { // start of public static void main
+		Scanner sc = new Scanner(System.in); // Scanner class
+		Mastermind m = new Mastermind(); // constructor for Mastermind
+		while (true) { // start of while loop
+			// gathers user input
 			ArrayList<Character> user_choice = new ArrayList<>();
-			System.out.println("There are 6 colors in total");
+			System.out.println("\nThere are 6 colors in total");
 			System.out.println("select 4 color of your choice");
 			System.out.println("red = R, green = G, blue = B, purple = P, orange = O, yellow = Y");
-			System.out.println("input a single character\n");
+			System.out.println("Example Input Format: R G B P");
+			System.out.print("Your input: ");
 			
 			for (int i = 0; i < 4; i ++) {
-				System.out.print((i + 1) + " color: ");
 				user_choice.add(sc.next().charAt(0));
 			}
-			m.check_correct(user_choice);
-		}
-		
-	}
+			// pass the user input to the Mastermind class
+			// Mastermind will check if the user input are right and print the board
+			if (m.check_correct(user_choice)) {
+				break;
+			}
+		} // end of while loop
+	} // end of public static void main
 } // end of Tester class
